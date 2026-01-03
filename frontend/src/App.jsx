55d99d9eb95adec303a334febbf2aa91601bef46
mo1kov-a <mo1kovanton04@gmail.com>
2026-01-03 12:17:22 +0300

    add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,13 @@
-const App = () => {};
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
